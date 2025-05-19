996ec91c8f5168b1400b4f5cef8b0165cd72cf97
niranjandahal <niranjandahal76@gmail.com>
2025-05-20 00:58:12 +0545

    final major project completed

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,64 +1,3 @@
-// import 'package:cbdc_admin/screens/signup_screen.dart';
-// import 'package:flutter/material.dart';
-// import 'nrb_dashboard.dart';
-// import 'bank_dashboard.dart';
-
-// class LoginScreen extends StatelessWidget {
-//   final bool isNRB;
-//   LoginScreen({required this.isNRB});
-
-//   final TextEditingController usernameController = TextEditingController();
-//   final TextEditingController passwordController = TextEditingController();
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar:
-//           AppBar(title: Text(isNRB ? "NRB Admin Login" : "Bank Admin Login")),
-//       body: Padding(
-//         padding: EdgeInsets.all(20),
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: [
-//             TextField(
-//                 controller: usernameController,
-//                 decoration: InputDecoration(labelText: "Username")),
-//             SizedBox(height: 20),
-//             TextField(
-//                 controller: passwordController,
-//                 decoration: InputDecoration(labelText: "Password"),
-//                 obscureText: true),
-//             SizedBox(height: 20),
-//             ElevatedButton(
-//               onPressed: () {
-//                 Navigator.pushReplacement(
-//                   context,
-//                   MaterialPageRoute(
-//                       builder: (context) =>
-//                           isNRB ? NRBDashboard() : BankDashboard()),
-//                 );
-//               },
-//               child: Text("Login"),
-//             ),
-//             SizedBox(height: 20),
-//             isNRB
-//                 ? Container()
-//                 : TextButton(
-//                     onPressed: () {
-//                       Navigator.pushReplacement(
-//                         context,
-//                         MaterialPageRoute(builder: (context) => SignupScreen()),
-//                       );
-//                     },
-//                     child: Text("Do a bank registration"),
-//                   ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
-
 import 'package:cbdc_admin/provider/bankprovider.dart';
 import 'package:cbdc_admin/screens/signup_screen.dart';
 import 'package:flutter/material.dart';
@@ -95,10 +34,15 @@ class _LoginScreenState extends State<LoginScreen> {
     });
 
     if (widget.isNRB) {
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => NRBDashboard()),
-      );
+      // if the username is nrb and password is nrbnepal123 then naviagate to dashboard.
+      if (email == "nrb" && password == "nrbnepal123") {
+        // Navigate to NRB Dashboard
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => NRBDashboard()),
+        );
+        return;
+      }
     } else {
       try {
         final bankProvider = Provider.of<BankProvider>(context, listen: false);
